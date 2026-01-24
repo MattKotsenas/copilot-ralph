b8ab7cec3579619b51c1faa5989663b6f6edeb98
Matt Kotsenas <matt.kotsenas@gmail.com>
2026-01-23 17:27:22 -0800

    Fix output encoding

--- src/Ralph.Cli/Program.cs
+++ src/Ralph.Cli/Program.cs
@@ -2,6 +2,10 @@
 // Entry point for the CLI application.
 
 using System.CommandLine;
+using System.Text;
 using Ralph.Cli.Commands;
+
+// Ensure console can display Unicode characters (including emojis and Braille art)
+Console.OutputEncoding = Encoding.UTF8;
 
 return CommandBuilder.Build().Parse(args).Invoke();
